ec4cc72dbbaca5d7347176b0236fa21b160351d9
DayaneErlacher <daayerlacher@gmail.com>
2022-05-22 18:44:32 -0300

    ajuste HTTPS

--- HTTPS/frontend/src/api/users.js
+++ HTTPS/frontend/src/api/users.js
@@ -1,7 +1,31 @@
 import axios from 'axios';
 
-const getUsers = () => {
+const getAllUser = () => {
     return (axios.get(`/api/users`));
 }
 
-export default getUsers;
+const registerUser = (data) => {
+    data = {
+        "name": "Laila",
+        "age": 78,
+        "address": "Av. Fernando Ferrari, 514 - Goiabeiras, Vitória - ES, 29075-910",
+        "cpf": "123.456.789-10"
+    }
+    return (axios.post(`/api/users`, data));
+}
+
+const updateUser = (data) => {
+    data.address = "Av. Fernando Ferrari, 514 - Goiabeiras, Vitória - ES, 29075-910";
+    data.age = 12;
+    return (axios.put(`/api/users`, data));
+}
+
+const deleteUser = (id) => {
+    return (axios.delete(`/api/users/${id}`));
+}
+
+const getByIdUser = (id) => {
+    return (axios.get(`/api/users/${id}`));
+}
+
+export { getAllUser, registerUser, updateUser, deleteUser, getByIdUser};
