c3a11ae2d5b0b898af705162c06bce12ec473cca
DayaneErlacher <daayerlacher@gmail.com>
2022-05-22 18:46:58 -0300

    ajuste GATEWAY front

--- GATEWAY/frontend/src/App.js
+++ GATEWAY/frontend/src/App.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import Users from './components/Users';
+import { FaPlus } from 'react-icons/fa';
+
+import { deleteUser, getAllUser, registerUser, updateUser } from './api/users';
 import Navbar from './components/Navbar';
-import getUsers from "./api/users";
+import Users from './components/Users';
+
 
 export default function App() {
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    getUsers().then(data => setUser(data.data))
+    getAllUser().then(data => setUser(data.data))
+    // getByIdUser(2).then(data => console.log(data.data))
   }, []);
 
   return (
     <>
     <Navbar titulo = "GATEWAY" />
-    <Users values = {user} ></Users>
+    <button onClick={() => registerUser()}><FaPlus /> Adicionar novo</button>
+    <Users values = {user} edit = {updateUser} delete={deleteUser} ></Users>
     </>
   );
 }
